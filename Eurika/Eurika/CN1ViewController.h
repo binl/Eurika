d35efc62de799c14b82e7d6868d1ad4281c0f7c5
harry <harrywenhr@gmail.com>
2012-11-01 19:43:46 -0400

    hiaoshdsa

---
+++ Eurika/Eurika/CN1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  CN1ViewController.h
+//  Eurika
+//
+//  Created by HAORAN WEN on 11/1/12.
+//  Copyright (c) 2012 CrushNuts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CN1ViewController : UIViewController
+
+@end
